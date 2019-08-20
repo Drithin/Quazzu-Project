054baa782e085e1b19b6a5e42c71e455e99dabb8
Drithin Chakravarthy Nellore <drithn@gmail.com>
2019-08-20 21:44:34 +0530

    Built the transparent navbar and herobox with content. Used CSS properties to align the content but got to work on responsiveness of it. Good first step. It needs more work to get a desired product

---
+++ JS/materialize.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('ul.right li').on('click', function(){
+        var clicked = $(this);
+        $('ul.right li').each(function(){
+            if($(this).hasClass('active')){
+                $(this).removeClass('active');
+            }
+        });
+        $(this).addClass('active');
+    });
+  });
+  
+  $(document).ready(function(){
+    $('ul.sidenav li').on('click', function(){
+        var clicked = $(this);
+        $('ul.sidenav li').each(function(){
+            if($(this).hasClass('active')){
+                $(this).removeClass('active');
+            }
+        });
+        $(this).addClass('active');
+    });
+  });
+
+  
